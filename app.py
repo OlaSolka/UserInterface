71315c73a337acd407c1ec33e6107c23c9a9eea6
OlaSolka <115350686+OlaSolka@users.noreply.github.com>
2024-01-14 16:14:46 +0100

    User registration app

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'], method='sha256')
+
+        new_user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('profile', username=email))
+
+    return render_template('register.html')
+
+@app.route('/profile/<username>', methods=['GET', 'POST'])
+def profile(username):
+    user = User.query.filter_by(email=username).first()
+
+    if request.method == 'POST':
+        # Aktualizacja danych profilu
+        user.first_name = request.form['first_name']
+        user.last_name = request.form['last_name']
+        db.session.commit()
+
+    return render_template('profile.html', user=user)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
